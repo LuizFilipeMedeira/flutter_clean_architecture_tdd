e9deab7228ac0c805341556970d0bd1c02935b5c
Luiz Filipe <luiz.fmdr@gmail.com>
2020-05-28 22:34:42 -0300

    adding repository contracts

---
+++ lib/features/number_trivia/data/datasources/number_trivia_local_data_source.dart
@@ -0,0 +1,13 @@
+
+import 'package:clean_architecture_tdd/core/error/exceptions.dart';
+import 'package:clean_architecture_tdd/features/number_trivia/data/models/number_trivia_model.dart';
+
+abstract class NumberTriviaLocalDataSource {
+  /// Get the cached [NumberTriviaModel] which was gotten the last time
+  /// the user had an internet connection
+  ///
+  /// Throws [CacheException] if no cached data is present.
+  Future<NumberTriviaModel> getLastNumberTrivia();
+
+  Future<void> cacheNumberTrivia(NumberTriviaModel triviaToCache);
+}
